be4d9f5ac4adebba410370884733c027863ffdc1
Ann <aashvareva@edu.hse.ru>
2026-01-10 23:58:46 +0300

    Implemented revocation of token

--- identity-service/go.mod
+++ identity-service/go.mod
@@ -5,8 +5,11 @@ go 1.24.0
 require github.com/gin-gonic/gin v1.11.0
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
@@ -34,6 +37,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/quic-go v0.54.0 // indirect
+	github.com/redis/go-redis/v9 v9.17.2
 	github.com/stretchr/testify v1.11.1
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
---
+++ identity-service/storage/revoke_token_storage.go
@@ -0,0 +1,59 @@
+package storage
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/karto4ki/karto4ki-backend/identity-service/jwt"
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	prefix = "RevokedToken"
+)
+
+var (
+	ErrRevokingToken        = errors.New("failed to revoke token")
+	ErrCheckTokenRevocation = errors.New("failed to check token revocation")
+)
+
+type RevokeStorage struct {
+	client *redis.Client
+	ttl    time.Duration
+}
+
+func NewRevokeStorage(clint *redis.Client, ttl time.Duration) *RevokeStorage {
+	return &RevokeStorage{
+		client: clint,
+		ttl:    ttl,
+	}
+}
+
+func (storage *RevokeStorage) Revoke(ctx context.Context, jwtToken jwt.Token) error {
+	key := storage.generateKey(jwtToken, prefix)
+	err := storage.client.Set(ctx, key, "revoked", storage.ttl).Err()
+	if err != nil {
+		return ErrRevokingToken
+	}
+	return nil
+}
+
+func (storage *RevokeStorage) IsRevoked(ctx context.Context, jwtToken jwt.Token) (bool, error) {
+	key := storage.generateKey(jwtToken, prefix)
+	exists, err := storage.client.Exists(ctx, key).Result()
+	if err != nil {
+		return false, ErrCheckTokenRevocation
+	}
+	return exists > 0, nil
+}
+
+func (storage *RevokeStorage) generateKey(jwtToken jwt.Token, prefix string) string {
+	hash := sha256.Sum256([]byte(jwtToken))
+	hashStr := hex.EncodeToString(hash[:])
+
+	return fmt.Sprintf("%s:%s", prefix, hashStr)
+}
